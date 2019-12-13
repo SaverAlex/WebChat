5eed96ce5fea168c7dd480bc08a35cfed994d7ff
EgorNmv <Egor_Nmv@bk.ru>
2019-12-13 23:30:32 +0500

    added js to main page

---
+++ src/main/resources/static/js/updatingChat.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch");
+const URL = "http://localhost:8080/session";
+const PING_INTERVAL = 1000;
+
+const updateChat = async () => {
+    try {
+        const response = await fetch(URL);
+        if (response.ok) {
+            const data = response.json();
+            const chatBody = document.querySelector(".chat");
+            data.data.length > 0 ?
+                data.data.forEach(item => (chatBody.append(`<div><b>${item.username}</b><span>${item.message}</span></div>`)))
+                : null;
+        }
+    } catch (e) {
+        console.info("error", e);
+    }
+};
+
+setInterval(updateChat, PING_INTERVAL);
