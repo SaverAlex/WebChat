cf84635934a59684c16e92afa7848740b27bbbfe
EgorNmv <Egor_Nmv@bk.ru>
2019-12-16 18:13:38 +0500

    lilfix js

--- src/main/resources/static/js/updatingChat.js
+++ src/main/resources/static/js/updatingChat.js
@@ -3,23 +3,30 @@ const PING_INTERVAL = 1000;
 
 const updateChat = async () => {
     try {
-        const response = await fetch(URL);
-        if (response.ok) {
-            const data = response.json();
-            const chatBody = document.querySelector(".chat");
-            data.data.length > 0 ?
-                data.data.forEach(item => (chatBody.append(`<div><b>${item.username}</b><span>${item.message}</span></div>`)))
-                : null;
-        }
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", URL, true);
+        await xhr.send();
+        const response = await xhr.response;
+        console.info("Response : ", response);
+
     } catch (e) {
-        console.info("error", e);
+        console.error("Response error", e);
     }
 };
 
-//setInterval(updateChat, PING_INTERVAL);
+setInterval(updateChat, PING_INTERVAL);
 
-setInterval(()=>{
-    const xhr = new XMLHttpRequest();
-     xhr.open("GET", URL, false);
-     xhr.send();
-     console.info("Ping!")},PING_INTERVAL);
+// const updateChat2 = async () => {
+//     try {
+//         const response = await fetch(URL);
+//         if (response.ok) {
+//             const data = response.json();
+//             const chatBody = document.querySelector(".chat");
+//             data.data.length > 0 ?
+//                 data.data.forEach(item => (chatBody.append(`<div><b>${item.username}</b><span>${item.message}</span></div>`)))
+//                 : null;
+//         }
+//     } catch (e) {
+//         console.info("error", e);
+//     }
+// };
